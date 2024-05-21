01d4e33430894e08212ccaf342edf9446a92b001
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-05-21 22:59:07 +0630

    [Complete] Add Services

---
+++ ConsoleApp.MiniToDoList/Services/InMemoryToDoRepository.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleApp.MiniToDoList.Interfaces;
+using ConsoleApp.MiniToDoList.Models;
+
+namespace ConsoleApp.MiniToDoList.Services;
+
+public class InMemoryToDoRepository : IToDoRepository
+{
+    private readonly List<ToDoItem> _items;
+
+    public InMemoryToDoRepository()
+    {
+        _items = new List<ToDoItem>();
+    }
+
+    public IEnumerable<ToDoItem> GetAll()
+    {
+        return _items.ToList();
+    }
+
+    public ToDoItem GetById(int id)
+    {
+        return _items.FirstOrDefault(x => x.Id == id);
+    }
+
+    public void Add(ToDoItem item)
+    {
+        item.Id = _items.Count + 1;
+        _items.Add(item);
+    }
+
+    public void Update(ToDoItem item)
+    {
+        var existingItem = GetById(item.Id);
+        if (existingItem != null)
+        {
+            existingItem.Description = item.Description;
+            existingItem.IsCompleted = item.IsCompleted;
+        }
+    }
+
+    public void Delete(int id)
+    {
+        var itemToDelete = GetById(id);
+        if (itemToDelete != null)
+        {
+            _items.Remove(itemToDelete);
+        }
+    }
+}
---
+++ ConsoleApp.MiniToDoList/Services/ToDoListService.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp.MiniToDoList.Interfaces;
+using ConsoleApp.MiniToDoList.Models;
+
+namespace ConsoleApp.MiniToDoList.Services;
+
+public class ToDoListService
+{
+    private readonly IToDoRepository _repository;
+
+    public ToDoListService(IToDoRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public IEnumerable<ToDoItem> GetTasks()
+    {
+        return _repository.GetAll();
+    }
+
+    public void AddTask(string description)
+    {
+        _repository.Add(new ToDoItem { Description = description });
+    }
+
+    public void MarkComplete(int id)
+    {
+        _repository.Update(new ToDoItem { Id = id, IsCompleted = true });
+    }
+
+    public void DeleteTask(int id)
+    {
+        _repository.Delete(id);
+    }
+}
