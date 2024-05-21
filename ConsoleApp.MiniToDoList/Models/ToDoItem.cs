98f34b404659cab04fa8b6a26f46aeb0167b638e
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-05-21 22:58:38 +0630

    [Complete] Add TodoModel

---
+++ ConsoleApp.MiniToDoList/Models/ToDoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp.MiniToDoList.Models;
+
+public class ToDoItem
+{
+    public int Id { get; set; }
+    public string Description { get; set; }
+    public bool IsCompleted { get; set; }
+}
