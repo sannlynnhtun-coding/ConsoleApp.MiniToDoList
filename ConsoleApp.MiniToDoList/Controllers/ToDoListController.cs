31d7a6abd9831fa89e1c296d1e26a486abfcee6a
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-05-21 22:59:21 +0630

    [Complete] Add ToDoController

---
+++ ConsoleApp.MiniToDoList/Controllers/ToDoListController.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleApp.MiniToDoList.Interfaces;
+using ConsoleApp.MiniToDoList.Models;
+using ConsoleApp.MiniToDoList.Services;
+
+namespace ConsoleApp.MiniToDoList.Controllers;
+
+public class ToDoListController
+{
+    private readonly ToDoListService _toDoListService;
+
+    public ToDoListController(ToDoListService toDoListService)
+    {
+        _toDoListService = toDoListService;
+    }
+
+    public IEnumerable<ToDoItem> GetTasks()
+    {
+        return _toDoListService.GetTasks();
+    }
+
+    public void AddTask(string description)
+    {
+        _toDoListService.AddTask(description);
+    }
+
+    public void MarkComplete(int id)
+    {
+        _toDoListService.MarkComplete(id);
+    }
+
+    public void DeleteTask(int id)
+    {
+        _toDoListService.DeleteTask(id);
+    }
+}
