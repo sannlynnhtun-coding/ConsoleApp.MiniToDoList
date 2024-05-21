a7c01d5fed472b5eb28f5d2030dcca977cb44fe9
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-05-21 22:58:56 +0630

    [Complete] Add Repository

---
+++ ConsoleApp.MiniToDoList/Interfaces/IToDoRepository.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleApp.MiniToDoList.Models;
+
+namespace ConsoleApp.MiniToDoList.Interfaces;
+
+public interface IToDoRepository
+{
+    IEnumerable<ToDoItem> GetAll();
+    ToDoItem GetById(int id);
+    void Add(ToDoItem item);
+    void Update(ToDoItem item);
+    void Delete(int id);
+}
